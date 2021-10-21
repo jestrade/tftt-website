7460f8d7cfc41648a733cb99217152149e46c984
Sergio Maury Sterling <psergio060897@gmail.com>
2021-10-21 14:42:33 -0500

    SignUp form

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,6 +7,7 @@ import { GlobalStyles } from '@styles/GlobalStyles'
 // Components
 import Layout from '@containers/Layout'
 import Home from '@pages/Home'
+import SignUpForm from '@components/SignUpForm'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Layout>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/signup' component={SignUpForm} />
         </Switch>
       </Layout>
     </BrowserRouter>
