f4ad58a849f08b22153ee285f9b8f7e35b0d9527
Yennifer Herrera <yenniferh@uninorte.edu.co>
2021-10-29 16:53:50 -0500

    UPD: Improve footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
             <h1>TALES FROM THE<span> TRAP</span></h1>
             <h2>FILM SYNOPSIS</h2>
             <p>TALES FROM THE TRAP is a gritty no holds barred look at the world faced by many UK's streets and council estates youths of today. A tale of gang culture, knife crime and cross county drug deals, however more than that TALES FROM THE TRAP is a story of brotherhood, loyalty and a friendship that runs deeper than any family.</p>
-            <p>Slick is a rising star in the UK Rap & Drill Music scenes as part of the SE8 crew who are tearing up the music scene, however with a rise in success also comes a rise in status and all the dangers that come with it. Soon the SE8’s friendship is tested to the limit as rival gangs, bent coppers, older drillers and dealers, fate and circumstances force these youths hand with deadly consequences.</p>
+            <p>Slick is a rising star in the UK Rap &amp; Drill Music scenes as part of the SE8 crew who are tearing up the music scene, however with a rise in success also comes a rise in status and all the dangers that come with it. Soon the SE8’s friendship is tested to the limit as rival gangs, bent coppers, older drillers and dealers, fate and circumstances force these youths hand with deadly consequences.</p>
             <p className='paragraph'>TALES FROM THE TRAP is as powerful and hard hitting as it is heartwarming and tells the story, which is all too familiar on today’s UK streets and council estates.</p>
             <SocialMediaBar />
             <img className='fade' src={fade} alt='Fade' />
@@ -45,9 +45,9 @@ const Home = () => {
             <img className='terryStone' src={terrystone} alt='Terry Stone' />
             <div className='terryStoneTextContainer'>
               <h1 className='terryTitle'>TERRY STONE</h1>
-              <p className='terryText'>“TALES FROM THE TRAP’s (“TFT<span>T</span>”) Film & Television Franchise will leverage from the anticipated exponential growing demand of digital assets in the Film, Television, social media, music and the graphic/art content industries.</p>
-              <p className='terryText'>TFT<span>T</span> will create a market place for the sale and resale of the Film & Television franchise assets and all related content through amongst others the highly sort after non fungible tokens. TFT<span>T</span> will commercialize through tokenomics and create digital intellectual property for the benefit of the TFT<span>T</span> loyal fanbase.”</p>
-              <p className='terryText2'>Anyone and everyone will have the opportunity to become a Part Shareholder in the TFT<span>T</span> Film & Television franchise.</p>
+              <p className='terryText'>“TALES FROM THE TRAP’s (“TFT<span>T</span>”) Film &amp; Television Franchise will leverage from the anticipated exponential growing demand of digital assets in the Film, Television, social media, music and the graphic/art content industries.</p>
+              <p className='terryText'>TFT<span>T</span> will create a market place for the sale and resale of the Film &amp; Television franchise assets and all related content through amongst others the highly sort after non fungible tokens. TFT<span>T</span> will commercialize through tokenomics and create digital intellectual property for the benefit of the TFT<span>T</span> loyal fanbase.”</p>
+              <p className='terryText2'>Anyone and everyone will have the opportunity to become a Part Shareholder in the TFT<span>T</span> Film &amp; Television franchise.</p>
               <h1 className='terrySubtitle'>PREVIOUS FILMS FROM TERRY STONE</h1>
               <div className='moviesContainer'>
                 <div className='imageWrapper'>
@@ -79,7 +79,7 @@ const Home = () => {
         <h1>IF YOU WATCHED...</h1>
         <div className='pictureContainer'>
           <div className='pictureContainer1'>"BLUE STORY",</div>
-          <div className='pictureContainer2'>"TOP BOY"&</div>
+          <div className='pictureContainer2'>"TOP BOY"&amp;</div>
           <div className='pictureContainer3'>"ADULTHOOD"</div>
         </div>
         <h2>YOUR IN THE RIGHT PLACE</h2>
