5ca818c734bc02691d54b2d540ef466b8d3180cb
Yennifer Herrera <yenniferh@uninorte.edu.co>
2021-11-01 19:46:37 -0500

    UPD: The Producer section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,49 +24,41 @@ export const About = () => {
           </h1>
         </div>
       </section>
-      <section>
-        <div className='gridProducer'>
-          <div className='title'>
-            <p>THE PRODUCER</p>
-            <p>Mr. STONE</p>
-          </div>
-          <div className='grid'>
-            <div>
-              <p>
-                Terry Stone is a world recognised, highly acclaimed,
-                Television/Film, BAFTA Nominee actor and producer, as well as
-                staring in one of the UK’s biggest and most successful crime
-                film franchises of all time.
-              </p>
-              <p>
-                Released in 2007, Terry created and starred in the inaugural
-                “Rise of the Footsoldier” the highly acclaimed British crime and
-                gangster film, pursuant to its success, Terry portrayed Tony
-                Tucker throughout the notorious “Rise of The Footsoldier” film
-                franchise, a franchise which has become one of the UK’s biggest
-                and most successful crime film franchises of all time.
-              </p>
-              <p>
-                Terry also portrayed starring roles in films such as the BAFTA
-                nominee “Rollin with The Nines” the UK crime drama “Bonded by
-                Blood” and he also starred in the UK box office smash
-                “Anuvahood”
-              </p>
-            </div>
-            <div>
-              Terry Stone commented: “I believe It’s just a matter of time
-              before most Television and Film productions adopt some form of
-              digital money as part of their own business modelling, the
-              Darkhorse team have developed a cryptocurrency and NFT’s
-              (Non-Fungible Tokens) platform that could potentially
-              revolutionise the way film production companies engage with fans
-              around the world, and together with ongoing revenues for all
-              stakeholders we have potentially developed an industry game
-              changer, we are full steam ahead and aim to launch the new
-              platform within the last Qtr. 2021”
-            </div>
-          </div>
-        </div>
+      <section className='producer'>
+        <h2>THE PRODUCER</h2>
+        <h3>Mr. TERRY STONE</h3>
+        <p>
+          Terry Stone is a world recognised, highly acclaimed,
+          Television/Film, BAFTA Nominee actor and producer, as well as
+          staring in one of the UK’s biggest and most successful crime
+          film franchises of all time.
+        </p>
+        <p>
+          Released in 2007, Terry created and starred in the inaugural
+          “Rise of the Footsoldier” the highly acclaimed British crime and
+          gangster film, pursuant to its success, Terry portrayed Tony
+          Tucker throughout the notorious “Rise of The Footsoldier” film
+          franchise, a franchise which has become one of the UK’s biggest
+          and most successful crime film franchises of all time.
+        </p>
+        <p>
+          Terry also portrayed starring roles in films such as the BAFTA
+          nominee “Rollin with The Nines” the UK crime drama “Bonded by
+          Blood” and he also starred in the UK box office smash
+          “Anuvahood”
+        </p>
+        <p>
+          Terry Stone commented: “I believe It’s just a matter of time
+          before most Television and Film productions adopt some form of
+          digital money as part of their own business modelling, the
+          Darkhorse team have developed a cryptocurrency and NFT’s
+          (Non-Fungible Tokens) platform that could potentially
+          revolutionise the way film production companies engage with fans
+          around the world, and together with ongoing revenues for all
+          stakeholders we have potentially developed an industry game
+          changer, we are full steam ahead and aim to launch the new
+          platform within the last Qtr. 2021”
+        </p>
       </section>
       <section className='bgBricks'>
         <section>
@@ -86,7 +78,7 @@ export const About = () => {
                   family.
                 </p>
                 <p>
-                  Slick is a rising star in the UK Rap & Drill Music scenes as
+                  Slick is a rising star in the UK Rap &amp; Drill Music scenes as
                   part of the SE8 crew who are tearing up the music scene, however
                   with a rise in success also comes a rise in status and all the
                   dangers that come with it. Soon the SE8’s friendship is tested
@@ -109,17 +101,17 @@ export const About = () => {
 
             <div className='textBox'>
               <div className='title'>
-                YOU CAN BECOME A SHAREHOLDER IN THE FILM & TV FRANCHISE
+                YOU CAN BECOME A SHAREHOLDER IN THE FILM &amp; TV FRANCHISE
               </div>
               <p>
-                “TALES FROM THE TRAP’s (“TFTT”) Film & Television Franchise will
+                “TALES FROM THE TRAP’s (“TFTT”) Film &amp; Television Franchise will
                 leverage from the anticipated exponential growing demand of digital
                 assets in the Film, Television, social media, music and the
                 graphic/art content industries.
               </p>
               <p>
                 TFTT will create a market place for
-                the sale and resale of the Film & Television franchise assets and
+                the sale and resale of the Film &amp; Television franchise assets and
                 all related content through amongst others the highly sort after non
                 fungible tokens. TFTT will commercialize through tokenomics and
                 create digital intellectual property for the benefit of the TFTT
@@ -127,7 +119,7 @@ export const About = () => {
               </p>
               <p>
                 TFTT will create a market place for
-                the sale and resale of the Film & Television franchise assets and
+                the sale and resale of the Film &amp; Television franchise assets and
                 all related content through amongst others the highly sort after non
                 fungible tokens. TFTT will commercialize through tokenomics and
                 create digital intellectual property for the benefit of the TFTT
