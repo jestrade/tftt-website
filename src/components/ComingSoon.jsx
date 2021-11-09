80c5084d48727070fd148f183d8cc26efe24ce25
Sergio Maury Sterling <psergio060897@gmail.com>
2021-11-09 14:45:46 -0500

    coming soon

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
 /* Styles */
-import { CountdownContainer, CustomButtonTitle, CountdownTimer, CountdownTitleContainer, CountdownTitle } from '@styles/ComingSoonStyles'
+import { CountdownContainer, CustomButtonTitle, CountdownTitleContainer, CountdownTitle } from '@styles/ComingSoonStyles'
 
 export const ComingSoon = (props) => {
   return (
     <CountdownContainer>
-      <CountdownTimer>
-        <CountdownTitleContainer>
-          <CountdownTitle>COMING<br />SOON</CountdownTitle>
-          <CustomButtonTitle>{props.name}</CustomButtonTitle>
-        </CountdownTitleContainer>
-      </CountdownTimer>
+      <CountdownTitleContainer>
+        <CountdownTitle>COMING<br />SOON</CountdownTitle>
+        <CustomButtonTitle>{props.name}</CustomButtonTitle>
+      </CountdownTitleContainer>
     </CountdownContainer>
   )
 }
